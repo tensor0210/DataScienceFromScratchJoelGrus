43eb8630f810799442caa79ce85da2c2ebc90479
Vidit Shah <ml02100@yahoo.com>
2020-12-28 15:41:58 +0530

    Sets

---
+++ 2_CrashCourseInPython/Sets.py
@@ -0,0 +1,14 @@
+prime_below_10 ={2,3,5,7}
+s= set()
+s.add(1)
+s.add(2)
+s.add(3)
+x =len(s)
+y = 2 in s
+z = 3 in s
+
+item+list =[1,2,3,1,2,3]
+num_items =len(item_list)
+item_set = set(item_list)
+num_distinct_items =len(item_set)
+distinct_item_list = list(item_set)
