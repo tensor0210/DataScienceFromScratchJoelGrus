6b32411ac46f3d9e5f5eb2ceec022369055e4683
Vidit Shah <ml02100@yahoo.com>
2020-12-28 14:55:29 +0530

    Tuples

---
+++ 2_CrashCourseInPython/Tuples.py
@@ -0,0 +1,15 @@
+my_list =[1,2]
+my_tuple =(1,2)
+other_tuple =3,4
+my_list[1] = 3
+
+try:
+  my_tuple[1] = 3
+except TypeError:
+  print("Cannot Modify a Tuple")
+
+def sum_and_product(x,y):
+  return (x+y),(x*y)
+
+sp = sum_and_product(2,3) #sp is (5,6)
+s,p = sum_and_product(5,10) #s is 15,p is 50
